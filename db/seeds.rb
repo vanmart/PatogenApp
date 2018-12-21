bb7dc6b590a7eca38592699a20822482077f8711
Ivan Martinez C <vanmartc@gmail.com>
2018-12-20 23:53:00 -0500

    api seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,3 @@
 
 Doorkeeper::Application.create(name: "patogenapp", uid: "542ec9b0fc71ee7ed2dd0efd513358c64f4c0fb419a49112ed78b0cc838dc096", secret: "17c9f5e7ad7481c7d1fed79407d9783f7cd58b6ae14af50e5cce04882bdb11ba", redirect_uri: "urn:ietf:wg:oauth:2.0:oob")
 
-Role.create([{ name: 'Admin' }, { name: 'MasterAdmin' }, { name: 'RegularUser' }, { name: 'IngAgronomo' }, { name: 'Agricultor' }])
-
-masterAdmin = User.create([{
-              name: 'van',
-              phone: '555',
-              email: 'vansteradmin@mail.com',
-              password: '1234321',
-              roles: [Role.find_by_name('MasterAdmin')]
-              },
-              {
-              name: 'Agustin',
-              phone: '555',
-              email: 'aguscalderonq@gmail.com',
-              password: 'patogenapp',
-              roles: [Role.find_by_name('Admin')]
-              },
-              {
-              name: 'Daniel',
-              phone: '555',
-              email: 'lopezdaniel1392@gmail.com',
-              password: 'patogenapp',
-              roles: [Role.find_by_name('Admin')]
-              }]
-              )
